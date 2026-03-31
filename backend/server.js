5994a4e4e6488f1ac316f28fe67a8ff823d5b9f3
Skprajapati235 <prajapatisonu7897@gmail.com>
2026-03-31 20:53:27 +0530

    fixed changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 const authMiddleware = require("./middleware/authMiddleware");
 const adminMiddleware = require("./middleware/adminMiddleware");
@@ -13,6 +14,10 @@ app.use(express.json());
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send("Backend is Live 🚀");
+});
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/admin/auth", require("./routes/admin/authRoutes"));
 app.use("/api/category", require("./routes/categoryRoutes"));
@@ -20,6 +25,7 @@ app.use("/api/product", require("./routes/productRoutes"));
 app.use("/api/upload", adminMiddleware, require("./routes/uploadRoutes"));
 app.use("/api/admin/users", adminMiddleware, require("./routes/userRoutes"));
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on ${process.env.PORT}`)
+
+app.listen(PORT, () =>
+  console.log(`Server running on ${PORT}`)
 );
