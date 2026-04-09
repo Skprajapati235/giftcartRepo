89dc7ffca3b843e127d3fc31f681bb88d5e2d57d
Skprajapati235 <prajapatisonu7897@gmail.com>
2026-04-09 22:24:18 +0530

    fixed image

--- mobile/src/screens/AddReviewScreen.js
+++ mobile/src/screens/AddReviewScreen.js
@@ -19,7 +19,7 @@ export default function AddReviewScreen({ route, navigation }) {
     }
 
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaType.Images,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.5,
